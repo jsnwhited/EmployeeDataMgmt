ac82dce5f26f04fdd469d810bb07227160014f04
MPA <50469825+MPA120@users.noreply.github.com>
2019-08-06 20:45:35 -0400

    debugging on click

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,29 @@
-$(placeholder).on('click', function(){
+var firebaseConfig = {
+    apiKey: "AIzaSyCi_R-FTtu6o7UHuZ0pqlYex6jL55394Jw",
+    authDomain: "employee-unc-ex-17.firebaseapp.com",
+    databaseURL: "https://employee-unc-ex-17.firebaseio.com",
+    projectId: "employee-unc-ex-17",
+    storageBucket: "",
+    messagingSenderId: "492632612991",
+    appId: "1:492632612991:web:1ca163027c7fdb80"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+$('#submit').on('click', function(){
+    console.log('click')
     database.ref().push({
-    name:$(name).val().trim(),
-    role:$(role).val().trim(),
-    startdate: $(startdate).val().trim(),
-    monthlyrate: $(monthlyrate).val().trim()
+    name:$('#name-input').val().trim(),
+    role:$('#role-input').val().trim(),
+    startdate: $('#date-input').val().trim(),
+    monthlyrate: $('#rate-input').val().trim()
 })})
 
-database.ref().on('value', function(snapshot){
-    var query = firebase.database().ref('users');
-    query.once('value')
-        .then(function(snapshot){
-            snapshot.forEach(function(childSnapshot) {
-                var key = childSnapshot.key;
-                var childData = childSnapshot.val();
-            })})})
+// database.ref().on('value', function(snapshot){
+//     var query = firebase.database().ref('users');
+//     query.once('value')
+//         .then(function(snapshot){
+//             snapshot.forEach(function(childSnapshot) {
+//                 var key = childSnapshot.key;
+//                 var childData = childSnapshot.val();
+//             })})})
